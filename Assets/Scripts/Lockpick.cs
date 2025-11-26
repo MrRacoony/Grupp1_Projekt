ec53bf2aa449cc48b11fc6ab674223db59c4f470
hugamana <hugo.eklind1@gmail.com>
2025-11-26 15:51:49 +0100

    Lockpick puzzle (not finished)

---
+++ Assets/Scripts/Lockpick.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Lockpick : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform verticalCast;
+
+    private Rigidbody2D rgbd;
+    private float mouseXPos;
+    public float minX;
+    public float maxX;
+    private float verticalRay = 1.35f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mouseXPos =  Mathf.Clamp(mousePos.x, minX, maxX);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseXPos, mousePos.y, Camera.main.nearClipPlane));
+
+        transform.position = new Vector2(worldPos.x, transform.position.y);
+
+        if (Input.GetMouseButtonDown(0)) {
+            CheckLock();
+        }
+        
+        CheckLock();
+        
+    }
+
+    private void CheckLock() {
+        RaycastHit2D upHit = Physics2D.Raycast(verticalCast.position, Vector2.up, verticalRay);
+
+        Vector2 origin = transform.position;
+        Debug.DrawRay(origin, Vector2.right * 10f, Color.red);
+
+        if(upHit.collider != null && upHit.collider.CompareTag("Lock")) {
+            Debug.Log("Hit");
+        }
+    }
+
+}
