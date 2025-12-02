af5eaeb3bc52a6772583e566903ed10556ab5541
hugamana <hugo.eklind1@gmail.com>
2025-12-02 10:52:35 +0100

    Fully implemented padlock graphics and sounds

--- Assets/Scripts/Padlock/PadlockNumber.cs
+++ Assets/Scripts/Padlock/PadlockNumber.cs
@@ -4,18 +4,20 @@ public class PadlockNumber : MonoBehaviour
 {
 
     [SerializeField] private int correctNum;
-
-    private Animator anim;
+    [SerializeField] private AudioClip scrollSound;
 
     private int currentNum;
     
     private bool isCorrect;
 
+    private Animator anim;
+    private AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
 
         currentNum = 0;
         if(currentNum == correctNum) {
@@ -31,6 +33,7 @@ public class PadlockNumber : MonoBehaviour
     }
 
     public void SetNum(int num) {
+        audioSource.PlayOneShot(scrollSound, 1f);
         currentNum += num;
         if(currentNum == 10) {
             currentNum = 0;
