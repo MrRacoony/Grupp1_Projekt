68bf281a247f96921b62c90df5afde0d01c58b88
MrRacoony <joarhjelmrin1@gmail.com>
2025-12-02 15:45:36 +0100

    Update RandomSound.cs

--- Assets/Scripts/RandomSound.cs
+++ Assets/Scripts/RandomSound.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class RandomSound : MonoBehaviour
 {
-    public GameAssets.SoundAudioClip[] audioClips;
-    private GameAssets.SoundAudioClip activeClip;
+    public SoundManager.Sound[] audioClips;
+    private SoundManager.Sound activeClip;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))//test function
@@ -15,6 +15,6 @@ public class RandomSound : MonoBehaviour
     public void PlayRandomSound()
     {
         activeClip = audioClips[Random.Range(0, audioClips.Length)];
-        SoundManager.PlaySound(activeClip.sound);
+        SoundManager.PlaySound(activeClip);
     }
 }
