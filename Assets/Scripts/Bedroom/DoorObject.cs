5b479c15cb8821d832aa02fb71d1d39b893508b5
hugamana <hugo.eklind1@gmail.com>
2025-12-04 11:21:13 +0100

    Door almost finished, demo scene addedw

--- Assets/Scripts/Bedroom/DoorObject.cs
+++ Assets/Scripts/Bedroom/DoorObject.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class DoorObject : MonoBehaviour
 {
 
     [SerializeField] private GameObject inventory;
+    [SerializeField] private string nextScene;
+
+    private string currentScene;
 
     private bool isOpen;
 
@@ -12,14 +18,9 @@ public class DoorObject : MonoBehaviour
     // Update is called once per frame
     void Start()
     {
+        currentScene = SceneManager.GetActiveScene().name;
         anim = GetComponent<Animator>();
         isOpen = false;
-    }
-
-    void Update() {
-        if(anim.GetBool("isOpen") != isOpen) {
-            anim.SetBool("isOpen", isOpen);
-        }
     }
 
     private void OnMouseDown()
@@ -27,14 +28,22 @@ public class DoorObject : MonoBehaviour
         inventory = GameObject.Find("Inventory");
         if(inventory.GetComponent<InventorySystem>().HasObject("BedroomKey") && !isOpen) {
             SoundManager.PlaySound(SoundManager.Sound.DoorOpening);
-            isOpen = true;
+            isOpen = true;  
             anim.SetBool("isOpen", isOpen);
         }
         else if(isOpen) {
             //scene change here
+            SceneController.OpenSceneAddition(nextScene);
+            SceneController.CloseSceneTemporary(currentScene);
         }
         else {
             SoundManager.PlaySound(SoundManager.Sound.PadlockUnlocked);
+        }
+    }
+
+    private void OnBecameVisible() {
+        if(anim.GetBool("isOpen") != isOpen) {
+            anim.SetBool("isOpen", isOpen);
         }
     }
 
