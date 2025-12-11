b3fb2830fd7e7574b6830eff208b64305c294cdf
hugamana <hugo.eklind1@gmail.com>
2025-12-11 11:45:13 +0100

    Added base for intro scene, added base for intersection room, updated radio dial script

--- Assets/Scripts/Radio/RadioDial.cs
+++ Assets/Scripts/Radio/RadioDial.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class RadioDial : MonoBehaviour
 {
+
+    private Vector3 mouse_pos;
+    private Transform target; //Assign to the object you want to rotate
+    private Vector3 object_pos;
+    private float angle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +17,13 @@ public class RadioDial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23F; //The distance between the camera and object
+        object_pos = Camera.main.WorldToScreenPoint(transform.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 
     private void OnMouseDown() {
