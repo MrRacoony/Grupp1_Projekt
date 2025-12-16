c461b9270a28b94c79ffa84408226e42512328e4
MrRacoony <joarhjelmrin1@gmail.com>
2025-12-16 13:14:40 +0100

    fixed

--- Assets/Scripts/Dialogue/Triggers/StartTrigger.cs
+++ Assets/Scripts/Dialogue/Triggers/StartTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class StartTrigger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
         GetComponent<DialogueTrigger>().TriggerDialogue();
     }
