fa530d9c473410ff44e78b154a24b3594284f9f7
hugamana <hugo.eklind1@gmail.com>
2025-12-17 10:31:59 +0100

    Added radio dial graphics + adjusted radio sounds

--- Assets/Scripts/Radio/RadioDial.cs
+++ Assets/Scripts/Radio/RadioDial.cs
@@ -14,16 +14,6 @@ public class RadioDial : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SoundManager.PlaySound(SoundManager.Sound.RadioStation1);
-        SoundManager.PlaySound(SoundManager.Sound.RadioStation2);
-        SoundManager.PlaySound(SoundManager.Sound.RadioStation3);
-        SoundManager.PlaySound(SoundManager.Sound.RadioStation4);
-        SoundManager.PlaySound(SoundManager.Sound.RadioStation5);
-        SoundManager.SetVolume(SoundManager.Sound.RadioStation1, 0f);
-        SoundManager.SetVolume(SoundManager.Sound.RadioStation2, 0f);
-        SoundManager.SetVolume(SoundManager.Sound.RadioStation3, 0f);
-        SoundManager.SetVolume(SoundManager.Sound.RadioStation4, 0f);
-        SoundManager.SetVolume(SoundManager.Sound.RadioStation5, 0f);
         isDragging = false;
         
     }
