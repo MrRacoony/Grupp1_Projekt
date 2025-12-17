d8bb743401cfb7540a9367393289e57763fe04f5
MrRacoony <joarhjelmrin1@gmail.com>
2025-12-17 15:31:02 +0100

    Update ChessManager.cs

--- Assets/Scripts/Puzzles/Chess/ChessManager.cs
+++ Assets/Scripts/Puzzles/Chess/ChessManager.cs
@@ -27,6 +27,7 @@ public class ChessManager : MonoBehaviour
     public void SetCurrentPiece(GameObject piece) {
         if (piece != currentPiece)
         {
+            currentPiece.GetComponent<SpriteRenderer>().color = baseColor;
             currentPiece = piece;
             baseColor = currentPiece.GetComponent<SpriteRenderer>().color;
             currentPiece.GetComponent<SpriteRenderer>().color = Color.cyan;
