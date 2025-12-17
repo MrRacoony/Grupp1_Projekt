8cc1d6fbea2d5ae86911b9f5f4ead33a51f0ac66
MrRacoony <joarhjelmrin1@gmail.com>
2025-12-17 14:57:16 +0100

    mouse fix

--- Assets/Scripts/General/Navigation.cs
+++ Assets/Scripts/General/Navigation.cs
@@ -5,6 +5,7 @@ public class Navigation : MonoBehaviour
 
     [SerializeField] private string nextScene;
     [SerializeField] private Texture2D arrowCursor;
+    [SerializeField] private Vector2 circleCursor = new Vector2(72, 72);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +14,7 @@ public class Navigation : MonoBehaviour
     }
 
     private void OnMouseEnter() {
-    Cursor.SetCursor(arrowCursor, Vector2.zero, CursorMode.Auto);
+    Cursor.SetCursor(arrowCursor, new Vector2(arrowCursor.width, arrowCursor.height), CursorMode.Auto);
     }
 
     private void OnMouseDown() {
@@ -27,11 +28,11 @@ public class Navigation : MonoBehaviour
         }
         SoundManager.PlaySound(SoundManager.Sound.UIClick);
         SceneController.OpenSceneAddition(nextScene);
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(null, circleCursor, CursorMode.Auto);
     }
 
     private void OnMouseExit() {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(null, circleCursor, CursorMode.Auto);
     }
 
 }
